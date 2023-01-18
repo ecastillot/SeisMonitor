41c0ba01456cf2f635545cde287bad457f02fb69
ecastillot <ecastillot@unal.edu.co>
2023-01-17 22:52:53 -0500

    init33

--- SeisMonitor/monitor/picker/utils.py
+++ SeisMonitor/monitor/picker/utils.py
@@ -43,7 +43,33 @@ from obspy.core.event.base import (QuantityError,
                                 WaveformStreamID,
                                 CreationInfo,
                                 Comment)
+from git import Repo
 from obspy.core.event import ResourceIdentifier
+
+
+def git_clone_aipicker(name,repo_dir):
+    """
+    Params:
+    -------
+    name: str
+        EQTransformer or PhaseNet
+    repo_dir: str
+        Directory path to place the repository
+    """
+    if name == "PhaseNet":
+        git_url = "https://github.com/ecastillot/PhaseNet.git"
+    elif name == "EQTransformer":
+        git_url = "https://github.com/ecastillot/EQTransformer.git"
+    else:
+        return False
+
+    repo_dir = os.path.join(repo_dir,name)
+    if os.path.isdir(repo_dir):
+        print("There is alaready")
+        return True
+    else:
+        Repo.clone_from(git_url, repo_dir)
+        return True
 
 ## EQTransformer functions
 def eqt_picks_2_seismonitor_fmt(eqt_folder,mseed_folder,out_path):
